ac84e3d3e32aaa0b28dc160035982cd317fd4aa1
Dessie2 <varguezdessire85@gmail.com>
2025-05-10 13:08:20 -0600

    fix: correct syntax errors in placeOrder function and improve line_items structure

--- back-end/controllers/orderController.js
+++ back-end/controllers/orderController.js
@@ -6,7 +6,9 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 //placing user order for front-end
 const placeOrder = async (req, res) => {
-    
+     
+     const frontend_url = "http://localhost:5173"
+
     try{
         const newOrder = new orderModel({
             userId:req.body.userId,
@@ -23,13 +25,30 @@ const placeOrder = async (req, res) => {
            product_data:{
             name:item.name
            },
-           
-         }
+           unit_amount:item.price*100*80
+         },
+          quantity:item.quantity
         }))
+        line_items.push({
+            price_data:{
+                currency:"inr",
+                product_data:{
+                    name:"Delivery data"
+                },
+                quantity:1
+            }
+        })
 
-    }catch(error){
-
+        const session = await stripe.checkout.sessions.creat({
+            line_items:line_items,
+            mode: "payment",
+            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+             cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+        });
+        res.json({success:true, session_url:session.url})
+    } catch (error) {
+        console.log({success:false, message:"Error"})
     }
-}
 
+}
 export {placeOrder}
